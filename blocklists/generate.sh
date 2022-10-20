cee11f74430f77ce5289b5b567b7a06089faab74
Tommaso Fonda <fondatommaso2@gmail.com>
2022-10-20 11:34:08 +0200

    block some more stuff

--- blocklists/generate.sh
+++ blocklists/generate.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 rm hosts
 touch hosts
-BASEURL="https://raw.githubusercontent.com/jmdugan/blocklists/master/corporations/"
+BASEURL="https://raw.githubusercontent.com/jmdugan/blocklists/master/corporations"
 
-declare -a corporations=("apple" "amazon" "pinterest")
+declare -a corporations=("apple" "amazon" "pinterest" "twitter")
 for corporation in "${corporations[@]}"
 do
-	curl -sS "$BASEURL$corporation/all" >> hosts
+	curl -sS "$BASEURL/$corporation/all" >> hosts
 done
 
 curl -sS "$BASEURL/facebook/all-but-whatsapp" >> hosts
+curl -sS "$BASEURL/microsoft/linkedin" >> hosts
